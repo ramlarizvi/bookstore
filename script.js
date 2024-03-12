90f8e4971b3e88007680b399f01dc2830ad7bbd1
Ramla Rizvi <iamramlarizvi@gmail.com>
2024-03-12 21:42:38 +0530

    home section

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ window.onload = () =>{
     }
 }
 
+let swiperHome = new Swiper('.home__swiper' ,{
+    loop: true,
+    spaceBetween:-24,
+    grabCursor: true,
+    slidesPerView:'auto',
+    centeredSlides:'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -62,
+        }
+    }
+})
